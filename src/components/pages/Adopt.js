3b422471b7ab9caa5cb86f3e552ed3879f96faaf
Andre De Carolis <decarolis.dev@gmail.com>
2022-10-01 00:53:05 +0100

    added infinite scroll and filters

--- src/components/pages/Adopt.js
+++ src/components/pages/Adopt.js
@@ -1,10 +1,12 @@
 import api from '../../utils/api';
 import { useNavigate } from 'react-router-dom';
 import { useState, useEffect } from 'react';
-import ModalImages from '../layout/ModalImages';
+import { FaSearch } from 'react-icons/fa';
 import { BsSuitHeart, BsFillSuitHeartFill } from 'react-icons/bs';
 import { GiWeight, GiSandsOfTime } from 'react-icons/gi';
 import {
+  TbChevronRight,
+  TbChevronDown,
   TbGenderMale,
   TbGenderFemale,
   TbGenderBigender,
@@ -12,7 +14,10 @@ import {
   TbMapPin,
 } from 'react-icons/tb';
 
-/* css*/
+/* pages */
+import ModalImages from '../layout/ModalImages';
+
+/* css */
 import styles from './Adopt.module.scss';
 import stylesLoader from '../layout/Loader.module.scss';
 
@@ -20,76 +25,84 @@ import stylesLoader from '../layout/Loader.module.scss';
 import useFlashMessage from '../../hooks/useFlashMessage';
 
 function Home() {
-  const [pets, setPets] = useState([]);
   const [token] = useState(localStorage.getItem('token') || '');
-  const [loading, setLoading] = useState(true);
+  const [obj, setObj] = useState({ pets: [], page: 1, search: '', sort: -1 });
   const [favorites, setFavorites] = useState([]);
+  const [search, setSearch] = useState('');
+  const [filter, setFilter] = useState(false);
   const { setFlashMessage } = useFlashMessage();
   const navigate = useNavigate();
 
-  const helpState = (tempPets, tempFavorites = []) => {
-    setLoading(false);
-    setPets(tempPets);
-    setFavorites(tempFavorites);
-  };
+  console.log(obj);
 
   useEffect(() => {
-    let tempPets, tempFavorites;
+    const intersectionObserver = new IntersectionObserver(
+      entries => {
+        if (
+          entries.some(entry => entry.isIntersecting) &&
+          entries[0].isVisible &&
+          obj.pets.length !== obj.total &&
+          obj.pets.length !== 0
+        ) {
+          console.log(entries[0].isVisible);
+          console.log('to qui');
+          setObj(prev => ({ ...prev, page: prev.page + 1 }));
+        }
+      },
+      {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0,
+
+        /* required options*/
+        trackVisibility: true,
+        delay: 100, // minimum 100
+      },
+    );
+    intersectionObserver.observe(document.querySelector('#sentinela'));
+    return () => intersectionObserver.disconnect();
+  }, [obj.total, obj.pets.length]);
+
+  useEffect(() => {
     let mounted = true;
-    if (token && loading) {
+    if (token) {
       api
-        .get('pets')
+        .get('/users/favorites', {
+          headers: {
+            Authorization: `Bearer ${JSON.parse(token)}`,
+          },
+        })
         .then(response => {
-          tempPets = response.data.pets;
-        })
-        .then(() => {
-          api
-            .get('/users/favorites', {
-              headers: {
-                Authorization: `Bearer ${JSON.parse(token)}`,
-              },
-            })
-            .then(response => {
-              const fav = [];
-              for (let i = 0; i < response.data.pets.length; i++) {
-                fav.push(response.data.pets[i]._id);
-              }
-              tempFavorites = fav;
-              if (mounted) {
-                helpState(tempPets, tempFavorites);
-              }
-            });
-        })
-        .catch(() => {
-          if (mounted) {
-            setFlashMessage(
-              'Houve um problema ao processar sua solicitação, tente novamente mais tarde!',
-              'error',
-            );
+          const fav = [];
+          for (let i = 0; i < response.data.pets.length; i++) {
+            fav.push(response.data.pets[i]._id);
           }
-        });
-    } else if (!token && loading) {
-      api
-        .get('pets')
-        .then(response => {
           if (mounted) {
-            tempPets = response.data.pets;
-            helpState(tempPets);
-          }
-        })
-        .catch(() => {
-          if (mounted) {
-            setFlashMessage(
-              'Houve um problema ao processar sua solicitação, tente novamente mais tarde!',
-              'error',
-            );
+            setFavorites(fav);
           }
         });
     }
     return () => {
       mounted = false;
     };
-  }, [setFlashMessage, token, loading]);
+  }, [token]);
+
+  useEffect(() => {
+    let mounted = true;
+    const url = `pets?page=${obj.page}&sort=${obj.sort}&search=${obj.search}`;
+    api.get(url).then(response => {
+      if (mounted) {
+        setObj(prev => ({
+          ...prev,
+          pets: [...prev.pets, ...response.data.pets],
+          total: response.data.total,
+        }));
+      }
+    });
+    return () => {
+      mounted = false;
+    };
+  }, [obj.page, obj.search, obj.sort]);
 
   async function handleFavorites(id, action) {
     if (token) {
@@ -148,104 +161,164 @@ function Home() {
     }
   }
 
+  function handleSearch(e) {
+    setSearch(e.target.value);
+  }
+
+  function handleClickSearch() {
+    if (search !== obj.search) {
+      setObj({ ...obj, pets: [], page: 1, search: search });
+    }
+  }
+
+  function handleSelect(e) {
+    setObj({ ...obj, pets: [], page: 1, [e.target.name]: e.target.value });
+  }
+
   return (
     <section>
       <h1>Adote um Pet</h1>
-      {!loading ? (
-        <div className={styles.pet_container}>
-          {pets.length > 0 ? (
-            pets.map(pet => (
-              <div className={styles.pet_card} key={pet._id}>
-                <ModalImages
-                  classname={'home'}
-                  image={[pet.images[0], 0]}
-                  pet={pet}
-                />
-                <div className={styles.title}>
-                  <h3>{pet.name}</h3>
-                  {favorites.includes(pet._id) ? (
-                    <BsFillSuitHeartFill
-                      onClick={() => handleFavorites(pet._id, 'remove')}
-                      className={styles.red_heart}
-                    />
-                  ) : (
-                    <BsSuitHeart
-                      onClick={() => handleFavorites(pet._id, 'add')}
-                      className={styles.heart}
-                    />
-                  )}
-                </div>
-                <p>
-                  <span>
-                    <TbInfoCircle />
-                  </span>
-                  {` ${pet.type}, ${pet.specificType}`}
-                </p>
-                <p>
-                  <span>{sexSwitch(pet.sex)}</span>
-                  {` ${pet.sex}`}
-                </p>
-                <p>
-                  <span>
-                    <GiSandsOfTime />
-                  </span>
-                  {pet.years === 0
-                    ? ''
-                    : pet.years === 1
-                    ? ` ${pet.years} ano`
-                    : ` ${pet.years} anos`}
-                  {pet.years > 0 && pet.months > 0 ? ' e ' : ''}
-                  {pet.months === 0
-                    ? ''
-                    : pet.months === 1
-                    ? ` ${pet.months} mês`
-                    : ` ${pet.months} meses`}
-                  {pet.months === 0 && pet.years === 0
-                    ? ' A idade não foi informada'
-                    : ''}
-                </p>
-                <p>
-                  <span>
-                    <GiWeight />
-                  </span>
-                  {pet.weightKg === 0
-                    ? ''
-                    : pet.weightKg === 1
-                    ? ` ${pet.weightKg} quilo`
-                    : ` ${pet.weightKg} quilos`}
-                  {pet.weightKg > 0 && pet.weightG > 0 ? ' e ' : ''}
-                  {pet.weightG === 0
-                    ? ''
-                    : pet.weightG === 1
-                    ? ` ${pet.weightG} grama`
-                    : ` ${pet.weightG} gramas`}
-                  {pet.weightKg === 0 && pet.weightG === 0
-                    ? ' O peso não foi informado'
-                    : ''}
-                </p>
-                <p>
-                  <span>
-                    <TbMapPin />
-                  </span>
-                  {` ${pet.state},  ${pet.city}`}
-                </p>
-                <button
-                  className={styles.button_home}
-                  onClick={() => handleButtonDetails(pet._id)}
-                >
-                  Mais detalhes
-                </button>
-              </div>
-            ))
-          ) : (
-            <p>
-              Não há pets cadastrados ou disponíveis para adoção no momento!
-            </p>
-          )}
+      <div className={styles.filter_container}>
+        <div
+          className={styles.filter_title}
+          onClick={() => {
+            setFilter(prev => !prev);
+          }}
+        >
+          <p>
+            Filtrar{' '}
+            <span>{filter ? <TbChevronDown /> : <TbChevronRight />}</span>
+          </p>
         </div>
+        {filter && (
+          <>
+            <div className={styles.filters}>
+              <select name="sort" onChange={handleSelect}>
+                <option value={-1}>Mais recentes primeiro</option>
+                <option value={1}>Mais antigos primeiro</option>
+              </select>
+            </div>
+            <div className={styles.search}>
+              <input
+                type="text"
+                name="search"
+                placeholder="Pesquisar"
+                onChange={handleSearch}
+              />
+              <button onClick={handleClickSearch}>
+                <FaSearch />
+              </button>
+            </div>
+          </>
+        )}
+      </div>
+      {obj.total ? (
+        <>
+          <div className={styles.pet_container}>
+            {obj.pets.length > 0 ? (
+              obj.pets.map(pet => (
+                <div className={styles.pet_card} key={pet._id}>
+                  <ModalImages
+                    classname={'home'}
+                    image={[pet.images[0], 0]}
+                    pet={pet}
+                  />
+                  <div className={styles.title}>
+                    <h3>{pet.name}</h3>
+                    {favorites.includes(pet._id) ? (
+                      <BsFillSuitHeartFill
+                        onClick={() => handleFavorites(pet._id, 'remove')}
+                        className={styles.red_heart}
+                      />
+                    ) : (
+                      <BsSuitHeart
+                        onClick={() => handleFavorites(pet._id, 'add')}
+                        className={styles.heart}
+                      />
+                    )}
+                  </div>
+                  <p>
+                    <span>
+                      <TbInfoCircle />
+                    </span>
+                    {` ${pet.type}, ${pet.specificType}`}
+                  </p>
+                  <p>
+                    <span>{sexSwitch(pet.sex)}</span>
+                    {` ${pet.sex}`}
+                  </p>
+                  <p>
+                    <span>
+                      <GiSandsOfTime />
+                    </span>
+                    {pet.years === 0
+                      ? ''
+                      : pet.years === 1
+                      ? ` ${pet.years} ano`
+                      : ` ${pet.years} anos`}
+                    {pet.years > 0 && pet.months > 0 ? ' e ' : ''}
+                    {pet.months === 0
+                      ? ''
+                      : pet.months === 1
+                      ? ` ${pet.months} mês`
+                      : ` ${pet.months} meses`}
+                    {pet.months === 0 && pet.years === 0
+                      ? ' A idade não foi informada'
+                      : ''}
+                  </p>
+                  <p>
+                    <span>
+                      <GiWeight />
+                    </span>
+                    {pet.weightKg === 0
+                      ? ''
+                      : pet.weightKg === 1
+                      ? ` ${pet.weightKg} quilo`
+                      : ` ${pet.weightKg} quilos`}
+                    {pet.weightKg > 0 && pet.weightG > 0 ? ' e ' : ''}
+                    {pet.weightG === 0
+                      ? ''
+                      : pet.weightG === 1
+                      ? ` ${pet.weightG} grama`
+                      : ` ${pet.weightG} gramas`}
+                    {pet.weightKg === 0 && pet.weightG === 0
+                      ? ' O peso não foi informado'
+                      : ''}
+                  </p>
+                  <p>
+                    <span>
+                      <TbMapPin />
+                    </span>
+                    {` ${pet.state},  ${pet.city}`}
+                  </p>
+                  <button
+                    className={styles.button_home}
+                    onClick={() => handleButtonDetails(pet._id)}
+                  >
+                    Mais detalhes
+                  </button>
+                </div>
+              ))
+            ) : (
+              <p>
+                Não há pets cadastrados ou disponíveis para adoção no momento!
+              </p>
+            )}
+          </div>
+        </>
       ) : (
         <div className={stylesLoader.loader}></div>
       )}
+      <div
+        style={
+          obj.pets.length === obj.total
+            ? { display: 'none' }
+            : { display: 'block' }
+        }
+        id="sentinela"
+      >
+        oi
+      </div>
     </section>
   );
 }
